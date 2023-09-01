867e901d28761d43d800b427483da55aaf0ab0e3
Anastasia <menshikova.naya@mail.ru>
2023-09-01 14:35:52 +0500

    убрать корс

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,18 @@ const { validationCreateUser, validationLogin } = require('../utils/regex');
 router.post('/signup', validationCreateUser, createUser);
 router.post('/signin', validationLogin, login);
 
-router.use(auth);
+//router.use(auth);
+"country": "menshikova.naya@mail.ru",
+"director": "password",
+"duration": 1000,
+"year": "password",
+ "description": "password",
+ "image": "https://shapka-youtube.ru/wp-content/uploads/2021/03/patsanskaya-kartinka-na-avu.jpg",
+ "trailerLink": "https://shapka-youtube.ru/wp-content/uploads/2021/03/patsanskaya-kartinka-na-avu.jpg",
+ "nameRU": "password",
+ "nameEN": "password",
+ "thumbnail": "https://shapka-youtube.ru/wp-content/uploads/2021/03/patsanskaya-kartinka-na-avu.jpg",
+ "movieId": 6457567
 
 router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
